8b9e34b8ebad06bac3b1ef65c09f9adf238d76db
Dinesh Raghavan <dineshraghavan29@gmail.com>
2023-03-31 13:35:15 +0530

    Add prod and dev configs

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,13 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 module.exports = {
-  mode: "development",
   entry: "./src/index.js",
   devtool: "none",
-  output: {
-    filename: "main.[contentHash].js",
-    path: path.resolve(__dirname, "dist"),
-  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
   module: {
     rules: [
       {
@@ -16,9 +16,4 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
 };
